c9cc9ff35c9f2ce91ac56016494713cabc32b1ea
sanskritikhande08 <khandesanskriti0@gmail.com>
2026-09-15 23:31:03 +0530

    Add initial portfolio files including HTML, CSS, JavaScript, and images

---
+++ script.js
@@ -0,0 +1,28 @@
+//dark
+ let themeBtn = document.getElementById("themeBtn");
+
+themeBtn.addEventListener("click", function() {
+
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+        themeBtn.innerHTML = "☀️";
+    }
+    else {
+        themeBtn.innerHTML = "🌙";
+    }
+
+});
+// Contact Form
+
+let form = document.getElementById("contactForm");
+
+form.addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    alert("Thank you! Your message has been submitted.");
+
+    form.reset();
+
+});
